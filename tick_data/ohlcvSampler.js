20413a2d38ff21722c8e18c7d8927171ec478dd0
Alex Moore <justkwer@gmail.com>
2023-11-02 17:10:56 +0300

    updates

--- tick_data/ohlcvSampler.js
+++ tick_data/ohlcvSampler.js
@@ -1,5 +1,6 @@
 const M = 1000000;
 const K = 1000;
+const min = 60;
 
 function getCountSize(count) {
   if (count > M) {
@@ -11,20 +12,28 @@ function getCountSize(count) {
   }
 }
 
+function getTime(count) {
+  if (count > min) {
+    return `${(count / min).toFixed(2)}m`;
+  } else {
+    return `${count.toFixed(2)}s`;
+  }
+}
+
 export default function sample(candles, foot, trade, vol_per_candle = 1000000) {
-  let last_candle = candles[candles.length - 1];
-  let last_foot = foot[candles.length - 1];
+  const last_candle = candles[candles.length - 1];
+  const last_foot = foot[candles.length - 1];
   if (!last_candle && !last_foot) return;
-  let tick = trade["price"];
-  let volume = trade["volume"] || 0;
-  let timestamp = trade["timestamp"];
+  const tick = trade["price"];
+  const volume = trade["volume"] || 0;
+  const timestamp = trade["timestamp"];
+  const duration = ((timestamp - last_candle[0]) / 1000);
 
   if (last_candle[5] > vol_per_candle) {
     // i want to go over volume then print new candle
     // And new zero-height candle
-    var duration = ((timestamp - last_candle[0]) / 1000).toFixed(2);
-    let nc = [timestamp, tick, tick, tick, tick, volume];
-    let nf = [timestamp, duration, volume];
+    const nc = [timestamp, tick, tick, tick, tick, volume];
+    const nf = [timestamp, duration, volume];
     //callback('candle-close', symbol)
     candles.push(nc);
     foot.push(nf);
@@ -35,7 +44,7 @@ export default function sample(candles, foot, trade, vol_per_candle = 1000000) {
     last_candle[4] = tick;
     last_candle[5] += volume;
     last_foot[2] = getCountSize(last_candle[5]);
-
+    last_foot[1] = getTime(duration);
     return false; // Make regular('update')
   }
 }
