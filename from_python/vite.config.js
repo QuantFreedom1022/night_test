bbd537a30ee8172c613ef4c4b5c19613befb90b3
Quant Freedom 1022 <quantfreedom1022@gmail.com>
2023-11-03 05:50:27 -0700

    updates

--- from_python/vite.config.js
+++ from_python/vite.config.js
@@ -38,15 +38,3 @@ export default ({ mode }) => {
     ],
   });
 };
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     svelte({
-//       emitCss: false,
-//     }),
-//     viteRawPlugin({
-//       fileRegex: /\.navy$/,
-//     })
-//   ]
-// })
