1d8d4d642a9b489ca2dc99f3d3c90e2c9312fe22
Alex Moore <justkwer@gmail.com>
2023-11-03 16:49:37 +0300

    feat(*): add delta table

--- tick_data/src/app.js
+++ tick_data/src/app.js
@@ -1,7 +1,7 @@
 import { NightVision } from 'night-vision';
 import data from '~data/candles.json';
 import FootStatsTable from '~navy/footStats.navy';
-import FootCandles from '~navy/footCandles.navy';
+import FootTable from '~navy/footTable.navy';
 import './style.css';
 import { setIntervalTicks } from '~utils';
 
@@ -11,7 +11,8 @@ let chart = new NightVision('chart-container', {
   data,
   autoResize: true,
   colors: { back: '#111113', grid: '#2e2f3055' },
-  scripts: [FootStatsTable, FootCandles],
+  scripts: [FootStatsTable, FootTable],
+  config: { CANDLEW: 0 },
 });
 
 window.chart = chart;
